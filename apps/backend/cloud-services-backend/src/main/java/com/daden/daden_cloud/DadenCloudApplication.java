75114743d436b0400b241450919bbc2dd063881a
danny <danny_karouw@hotmail.com>
2025-01-03 00:24:03 +0100

    opzet daden met omgeving

---
+++ apps/backend/cloud-services-backend/src/main/java/com/daden/daden_cloud/DadenCloudApplication.java
@@ -0,0 +1,13 @@
+package com.daden.daden_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadenCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadenCloudApplication.class, args);
+	}
+
+}
---
+++ apps/backend/cloud-services-backend/src/test/java/com/daden/daden_cloud/DadenCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daden.daden_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadenCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
